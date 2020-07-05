3f64bae4b0151b53a847962752155999263af168
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-07-05 12:36:51 -0400

    first commit, basic outline

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+//fwxUMVRWEwEB4QSXOKgUuUJcvg6CG3dk8B108l1W
+//https://api.nasa.gov/planetary/apod
